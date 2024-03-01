e9c585e935a42f96042e6f1968469e1b31713f46
proxy-o <you@example.com>
2024-03-01 18:20:55 +0100

    back to a simple list

--- src/app/[local]/(company)/admin/components/createCompanyForm.tsx
+++ src/app/[local]/(company)/admin/components/createCompanyForm.tsx
@@ -53,179 +53,144 @@ export default function CreateCompanyForm() {
   }
 
   return (
-    <Tabs defaultValue={t("company_info")} className="w-full">
-      <TabsList className="h-46 relative z-50 grid w-full grid-cols-2 gap-2 md:grid-cols-3">
-        <TabsTrigger value={t("company_info")} className="border">
-          {t("company_info")}
-        </TabsTrigger>
-        <TabsTrigger value={t("address_info")} className="border">
-          {t("address_info")}{" "}
-        </TabsTrigger>
-        <TabsTrigger value={t("contact_info")} className="border">
-          {t("contact_info")}
-        </TabsTrigger>
-        <TabsTrigger value={t("vat_number")} className="border">
-          {t("vat_number")}
-        </TabsTrigger>
-        <TabsTrigger value={t("bank_info")} className="border">
-          {t("bank_info")}
-        </TabsTrigger>
-        <TabsTrigger value={t("bank_info2")} className="border">
-          {t("bank_info2")}
-        </TabsTrigger>
-      </TabsList>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col items-center gap-2   p-2">
+          <Card className="flex min-w-full flex-col gap-6 p-6">
+            <p className="">{t("company_info")}</p>
+            <CustomField
+              control={form.control}
+              name="company_logo"
+              label={t("company_logo")}
+              placeholder={t("company_logo")}
+            />
+            <CustomField
+              control={form.control}
+              name="company_name"
+              label={t("company_name")}
+              placeholder={t("company_name")}
+            />
+            <div className="flex w-full gap-2 ">
+              <CustomField
+                control={form.control}
+                name="owner_name"
+                label={t("owner_name")}
+                placeholder={t("owner_name")}
+              />
+              <CustomField
+                control={form.control}
+                name="owner_lastname"
+                label={t("owner_lastname")}
+                placeholder={t("owner_lastname")}
+              />
+            </div>
+          </Card>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex flex-col items-center gap-2   p-2">
-            <TabsContent value={t("company_info")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 ">
-                <p className="">{t("company_info")}</p>
-                <CustomField
-                  control={form.control}
-                  name="company_logo"
-                  label={t("company_logo")}
-                  placeholder={t("company_logo")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="company_name"
-                  label={t("company_name")}
-                  placeholder={t("company_name")}
-                />
-                <div className="flex w-full gap-2 ">
-                  <CustomField
-                    control={form.control}
-                    name="owner_name"
-                    label={t("owner_name")}
-                    placeholder={t("owner_name")}
-                  />
-                  <CustomField
-                    control={form.control}
-                    name="owner_lastname"
-                    label={t("owner_lastname")}
-                    placeholder={t("owner_lastname")}
-                  />
-                </div>
-              </Card>
-            </TabsContent>
+          <Card className="flex min-w-full flex-col gap-6 p-6">
+            <p>{t("address_info")}</p>
+            <CustomField
+              control={form.control}
+              name="street"
+              label={t("street")}
+              placeholder={t("street")}
+            />
+            <CustomField
+              control={form.control}
+              name="zip_code"
+              label={t("zip_code")}
+              placeholder={t("zip_code")}
+            />
+            <div className="flex w-full gap-2 ">
+              <CustomField
+                control={form.control}
+                name="city"
+                label={t("city")}
+                placeholder={t("city")}
+              />
+              <CustomField
+                control={form.control}
+                name="country"
+                label={t("country")}
+                placeholder={t("country")}
+              />
+            </div>
+          </Card>
+          <Card className="flex min-w-full flex-col gap-6 p-6 ">
+            <p>{t("contact_info")}</p>
+            <CustomField
+              control={form.control}
+              name="owner_email"
+              label={t("owner_email")}
+              placeholder={t("owner_email")}
+            />
+            <CustomField
+              control={form.control}
+              name="owner_phone"
+              label={t("owner_phone")}
+              placeholder={t("owner_phone")}
+            />
+            <CustomField
+              control={form.control}
+              name="owner_website"
+              label={t("owner_website")}
+              placeholder={t("owner_website")}
+            />
+          </Card>
 
-            <TabsContent value={t("address_info")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 p-6">
-                <p>{t("address_info")}</p>
-                <CustomField
-                  control={form.control}
-                  name="street"
-                  label={t("street")}
-                  placeholder={t("street")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="zip_code"
-                  label={t("zip_code")}
-                  placeholder={t("zip_code")}
-                />
-                <div className="flex w-full gap-2 ">
-                  <CustomField
-                    control={form.control}
-                    name="city"
-                    label={t("city")}
-                    placeholder={t("city")}
-                  />
-                  <CustomField
-                    control={form.control}
-                    name="country"
-                    label={t("country")}
-                    placeholder={t("country")}
-                  />
-                </div>
-              </Card>
-            </TabsContent>
-            <TabsContent value={t("contact_info")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 p-6 ">
-                <p>{t("contact_info")}</p>
-                <CustomField
-                  control={form.control}
-                  name="owner_email"
-                  label={t("owner_email")}
-                  placeholder={t("owner_email")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="owner_phone"
-                  label={t("owner_phone")}
-                  placeholder={t("owner_phone")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="owner_website"
-                  label={t("owner_website")}
-                  placeholder={t("owner_website")}
-                />
-              </Card>
-            </TabsContent>
+          <Card className="flex min-w-full flex-col gap-6 p-6">
+            <CustomField
+              control={form.control}
+              name="vat_number"
+              label={t("vat_number")}
+              placeholder={t("vat_number")}
+            />
+          </Card>
+          <Card className="flex min-w-full flex-col gap-6 p-6">
+            <p>{t("bank_info")}</p>
+            <CustomField
+              control={form.control}
+              name="bank_name"
+              label={t("bank_name")}
+              placeholder={t("bank_name")}
+            />
+            <CustomField
+              control={form.control}
+              name="bank_account_number"
+              label={t("bank_account_number")}
+              placeholder={t("bank_account_number")}
+            />
+            <CustomField
+              control={form.control}
+              name="bic_number"
+              label={t("bic_number")}
+              placeholder={t("bic_number")}
+            />
+          </Card>
+          <Card className="flex min-w-full flex-col gap-6 p-6">
+            <p>{t("bank_info2")}</p>
+            <CustomField
+              control={form.control}
+              name="bank_name2"
+              label={t("bank_name2")}
+              placeholder={t("bank_name2")}
+            />
+            <CustomField
+              control={form.control}
+              name="bank_account_number2"
+              label={t("bank_account_number2")}
+              placeholder={t("bank_account_number2")}
+            />
+            <CustomField
+              control={form.control}
+              name="bic_number2"
+              label={t("bic_number2")}
+              placeholder={t("bic_number2")}
+            />
+          </Card>
+        </div>
 
-            <TabsContent value={t("vat_number")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 p-6">
-                <CustomField
-                  control={form.control}
-                  name="vat_number"
-                  label={t("vat_number")}
-                  placeholder={t("vat_number")}
-                />
-              </Card>
-            </TabsContent>
-            <TabsContent value={t("bank_info")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 p-6">
-                <p>{t("bank_info")}</p>
-                <CustomField
-                  control={form.control}
-                  name="bank_name"
-                  label={t("bank_name")}
-                  placeholder={t("bank_name")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="bank_account_number"
-                  label={t("bank_account_number")}
-                  placeholder={t("bank_account_number")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="bic_number"
-                  label={t("bic_number")}
-                  placeholder={t("bic_number")}
-                />
-              </Card>
-            </TabsContent>
-            <TabsContent value={t("bank_info2")} className="w-full">
-              <Card className="flex min-w-full flex-col gap-6 p-6">
-                <p>{t("bank_info2")}</p>
-                <CustomField
-                  control={form.control}
-                  name="bank_name2"
-                  label={t("bank_name2")}
-                  placeholder={t("bank_name2")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="bank_account_number2"
-                  label={t("bank_account_number2")}
-                  placeholder={t("bank_account_number2")}
-                />
-                <CustomField
-                  control={form.control}
-                  name="bic_number2"
-                  label={t("bic_number2")}
-                  placeholder={t("bic_number2")}
-                />
-              </Card>
-            </TabsContent>
-          </div>
-
-          <Button type="submit">Create User</Button>
-        </form>
-      </Form>
-    </Tabs>
+        <Button type="submit">{t("create_company")}</Button>
+      </form>
+    </Form>
   );
 }
