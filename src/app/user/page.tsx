b5ad16bf9f3b1203513374f45c4755527e048159
proxy-o <you@example.com>
2024-03-01 19:23:50 +0100

    make user page server side

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,14 +1,10 @@
-"use client";
-import { redirect, useRouter } from "next/navigation";
 import React from "react";
 import { validateRequest } from "~/server/lucia/validateRequests";
-import { api } from "~/trpc/server";
 import CreateClientForm from "./components/createClientForm";
-import { useSession } from "../_context/SessionContext";
 import ClientsList from "./components/clientsList";
 
-export default function Page() {
-  const { session, user } = useSession();
+export default async function Page() {
+  const { session, user } = await validateRequest();
 
   console.log(user);
   return (
