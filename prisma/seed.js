7eba05bfee60bcb683568974930527b81a79a08b
proxy-o <you@example.com>
2024-03-03 09:46:03 +0100

    seed data

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,53 +1,128 @@
 import { PrismaClient } from "@prisma/client";
-import { generateId } from "lucia";
 import { Argon2id } from "oslo/password";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.role.create({
+  // create sittings
+  await prisma.settings.create({
     data: {
-      name: "admin",
-    },
-  });
-
-  await prisma.role.create({
-    data: {
-      name: "user",
-    },
-  });
-
-  await prisma.type.create({
-    data: {
-      name: "cafe",
-    },
-  });
-
-  await prisma.type.create({
-    data: {
-      name: "gym",
-    },
-  });
-  await prisma.type.create({
-    data: {
-      name: "restaurant",
+      company_logo: "images/mbo.png",
+      company_name: "MBO",
+      owner_name: "Mehdi",
+      owner_lastname: "Latifi",
+      vat_number: "123.456.789",
+      street: "vivastreet 123",
+      zip_code: "1234",
+      city: "Hasselt",
+      country: "TEST",
+      owner_email: "mbo@test.be",
+      owner_phone: "04949002034",
+      owner_website: "www.mbo.be",
+      bank_name: "ING",
+      bank_account_number: "123443211234",
+      bic_number: "BNBTRH",
+      bank_name2: "KBC",
+      bank_account_number2: "BE1234569305",
+      bic_number2: "BNHJGNG",
     },
   });
 
   // create admin user
   const hashedPassword = await new Argon2id().hash("admin123");
-  const userId = generateId(15);
   await prisma.user.create({
     data: {
-      id: userId,
       username: "admin",
       email: "admin@gmail.com",
-      hashed_password: hashedPassword,
-      role: {
+      password: hashedPassword,
+      compagny: {
         connect: {
-          name: "admin",
+          id: 1,
         },
       },
+    },
+  });
+  await prisma.car.create({
+    data: {
+      chassis_number: "123ABC456DEF",
+      brand: "BMW",
+      model: "330",
+      car_type: "Personenwagen",
+      transmission: "Automatisch",
+      first_registration: new Date("2020-05-01"),
+      mileage: 1000,
+      engine_power: 200,
+      cylinder: 3,
+      fuel: "Benzine",
+      co2: "100",
+      color: "Zwart",
+      number_keys: 2,
+      cer_of_conf: true,
+      inspection_form: true,
+      car_pass: true,
+      register_cert: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+      compagny_id: 1, // Assuming you have a company with ID 1
+    },
+  });
+
+  await prisma.car.create({
+    data: {
+      chassis_number: "9238BCEF56DEF",
+      brand: "DACIA",
+      model: "330",
+      car_type: "Personenwagen",
+      transmission: "Automatisch",
+      first_registration: new Date("2020-05-01"),
+      mileage: 1000,
+      engine_power: 200,
+      cylinder: 3,
+      fuel: "Benzine",
+      co2: "150",
+      color: "Blue",
+      number_keys: 2,
+      cer_of_conf: true,
+      inspection_form: true,
+      car_pass: true,
+      register_cert: true,
+      compagny_id: 1,
+    },
+  });
+
+  await prisma.client.create({
+    data: {
+      is_company: false,
+      surname: "Doe",
+      firstname: "John",
+      company_name: null, // Assuming not a company
+      btw_number: null, // Assuming not applicable
+      street: "123 Main St",
+      postal_code: "12345",
+      city: "Anytown",
+      country: "USA",
+      email: "john.doe@example.com",
+      phone_number: "123-456-7890",
+      mobile_number: "098-765-4321",
+      compagny_id: 1, // Assuming you have a company with ID 1
+    },
+  });
+
+  await prisma.client.create({
+    data: {
+      is_company: false,
+      surname: "lila",
+      firstname: "bira",
+      company_name: null, // Assuming not a company
+      btw_number: null, // Assuming not applicable
+      street: "123 Main St",
+      postal_code: "12345",
+      city: "Anytownf",
+      country: "maroco",
+      email: "bira@example.com",
+      phone_number: "123-456-7890",
+      mobile_number: "098-765-4321",
+      compagny_id: 1, // Assuming you have a company with ID 1
     },
   });
 }
